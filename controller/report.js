5a30a182304bd807ba4cd372d21109f91b1ac884
weiwei97 <vincent.vv@qq.com>
2016-12-10 18:21:15 +0800

    daily & hour report update

--- controller/report.js
+++ controller/report.js
@@ -1,34 +1,68 @@
 var Dao = require('../dao/report');
-
+var Moment = require('moment');
+require('moment-range');
 
 var handler = {
 
   findByHour: function(req, res, next){
     var aid = req.params.aid,
         day = req.params.day;
+
+    var resObj = {
+      showArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      clickArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      moneyArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    };
+
     Dao.findByHour(aid, day, function(data){
-      var resObj = {
-        showArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        clickArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        moneyArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-      };
+      
       data.forEach(function(item){
-        switch(item.event_type){
-          case 0: //unknow
-          case 1: //bid
-            break;
-          case 2: //show
-            resObj.showArr[item.hour] = item.total;
-            break;
-          case 3: //click
-            resObj.clickArr[item.hour] = item.total;
-            break;
-        }
-        resObj.moneyArr[item.hour] += item.allmoney;
+        resObj.showArr[item.hour] = item.show_all;
+        resObj.clickArr[item.hour] = item.click_all;
+        resObj.moneyArr[item.hour] = item.money;
+      }) 
+      next(resObj);
+    });
+  },
+
+  findByDays: function(req, res, next){
+    var aid = req.params.aid;
+    var days = req.params.days.split('t');
+    var start = days[0],
+        end = days[1];
+    
+    Dao.findByDay(aid, start, end, function(data){
+
+      if(data.error){
+        return next(data);
+      }
+
+      // 获取区间所有日期
+      var rangeDays = Moment.range(Moment(start, "YYYY-MM-DD"), Moment(end, "YYYY-MM-DD")).toArray('days');
+
+      var day_key = '',
+          resObj = {};
+      for(var i=0;i<rangeDays.length;i++){
+        day_key = Moment(rangeDays[i]).format('YYYY-MM-DD');
+        resObj[day_key] = {
+          show: 0,
+          click: 0,
+          money: 0
+        };
+      }
+
+      data.forEach(function(item){
+        var day = Moment(item.day).format('YYYY-MM-DD');
+        var day_data = resObj[day];
+        day_data.show = item.show_all;
+        day_data.click = item.click_all;
+        day_data.money = item.money_all;
       })
+      
       next(resObj);
     });
   }
+
 }
 
 module.exports = handler;
--- dao/report.js
+++ dao/report.js
@@ -8,10 +8,13 @@ var report = {};
 report.findByHour =  function(aid, date, next){
 
   // 调试无数据，去掉advertiserid filter，上线需恢复
-  var sql = 'select sum(`num`) as total, sum(`money`) as allmoney, `event_type`, hour(logtime) as hour from traffic_consume_realtime WHERE ' +
-    // 'advertiserid = '+ aid +' AND '+
-    'logtime between "'+ date +' 00:00:00" and "' + date + ' 23:59:59" ' +
-    'group by `event_type`, hour(`logtime`) order by hour, event_type';
+
+  var sql = 'select sum(`ns`) as show_all, sum(`nc`) as click_all, sum(`money`) as money_all, hour(`logtime`) as hour '+
+            'from traffic_consume_realtime WHERE '+
+            // 'advertiserid = '+ aid +' AND '+
+            'logtime between "'+ date +' 00:00:00" and "' + date + ' 23:59:59" ' + 
+            'group by hour(`logtime`);'
+
   connection.query( sql, 
       function(err, rows, fields) {
           if (err) {
@@ -24,15 +27,23 @@ report.findByHour =  function(aid, date, next){
 }
 
 
-report.findByAid = function(aid, next){
-    connection.query('SELECT * FROM traffic_consume_realtime WHERE advertiser_id=' + aid +' ORDER BY opt_time DESC', 
-        function(err, rows, fields) {
-            if (err) {
-                next(err);                
-                throw err;
-            }
-            next(rows);
+report.findByDay = function(aid, start, end, next){
+
+  // 调试无数据，去掉advertiserid filter，上线需恢复
+  var sql = 'select sum(`ns`) as show_all, sum(`nc`) as click_all, sum(`money`) as money_all, date(`logtime`) as day '+
+            'from traffic_consume_realtime WHERE '+
+            // 'advertiserid = '+ aid +' AND '+
+            'logtime between "'+ start +' 00:00:00" and "' + end + ' 23:59:59" ' + 
+            'group by date(`logtime`);'
+
+    connection.query(sql, 
+      function(err, rows, fields) {
+        if (err) {
+            next(err);                
+            throw err;
         }
+        next(rows);
+      }
     );
 }
 
