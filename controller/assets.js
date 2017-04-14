b62e78f3d077ccfb9ba8744027e24f0fc17c8c88
weiwei97 <vincent.vv@qq.com>
2017-04-14 23:19:53 +0800

    domain debug

--- controller/assets.js
+++ controller/assets.js
@@ -2,5 +2,6 @@ var configs = require('../assets.config.js');
 
 module.exports = function(req, res, next){
   var domain = req.headers.host.split(':')[0];
-  next(configs[domain]);
+  configs.domain = req.headers.host;
+  next(configs);
 }
