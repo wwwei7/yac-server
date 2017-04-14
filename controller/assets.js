b785c740d14d99be1ebc1a7bb9d112b8dfe6361a
weiwei97 <vincent.vv@qq.com>
2017-04-14 23:54:10 +0800

    assets api

--- controller/assets.js
+++ controller/assets.js
@@ -2,6 +2,5 @@ var configs = require('../assets.config.js');
 
 module.exports = function(req, res, next){
   var domain = req.headers.host.split(':')[0];
-  configs.domain = req.headers;
-  next(configs);
+  next(configs[domain]);
 }
