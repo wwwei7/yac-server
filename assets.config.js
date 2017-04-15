502cb5edf97cb3daa60f26131fa14a746d191a59
weiwei97 <vincent.vv@qq.com>
2017-04-15 13:56:46 +0800

    ipang assets update

--- assets.config.js
+++ assets.config.js
@@ -5,13 +5,13 @@ module.exports = {
     icp: '粤ICP备16037727号-1'
   },
   'dsp.ipangchina.com': {
-    logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo.png',
+    logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo-.png',
     dname: '小胖看房',
     icp: '粤ICP备16037727号-1'
   },
   'localhost': {
     // logo: 'http://mt.youradcloud.com/web/logo-1.png',
-    logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo.png',    
+    logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo-.png',    
     dname: '小胖看房',
     icp: '粤ICP备16037727号-1'    
   }
