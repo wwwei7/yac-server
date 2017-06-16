832c296575723851d7345ccfc3a5607c30dfb584
CN1\wei.weib <wei.weib@Ctrip.com>
2017-06-16 13:15:55 +0800

    login err fix

--- controller/login.js
+++ controller/login.js
@@ -1,17 +1,22 @@
 var session = require('express-session');
 var User = require('../dao/user');
-
+var assets = require('../assets.config.js');
 
 var login = function(req, res, next){
     var name = req.body.username;
     var password = req.body.password;
+    var domain = req.headers.host.split(':')[0];
+    var assetsData = assets[domain];
 
     var callback = function(user){
         if(user){
             req.session.user = user;
             return res.redirect('/app');
         }else{
-            res.render('index', { err: '用户名或密码错误'});
+            res.render('index', { 
+                err: '用户名或密码错误',
+                assets: assets[domain]
+            });        
         }
     };
 
