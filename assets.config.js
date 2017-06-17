974ccaeac50a0e359583314ed1b73f168ffee531
vincent <waydewei@gamil.com>
2017-06-17 13:40:37 +0800

    yuppo 素材配置

--- assets.config.js
+++ assets.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'admin.youradcloud.com': {
     logo: 'http://mt.youradcloud.com/web/logo-1.png',
-    dname: '优效广告',
+    dname: '优效',
     icp: '粤ICP备16037727号-1'
   },
   'dsp.ipangchina.com': {
@@ -9,10 +9,15 @@ module.exports = {
     dname: '小胖看房',
     icp: '沪ICP备15005792号-2'
   },
+  'dsp.yuppo.cn': {
+    logo: 'http://mt.youradcloud.com/web/act/yunpu/logo-yunpu.png',
+    dname: '云蒲',
+    icp: ''
+  },
   'localhost': {
-    // logo: 'http://mt.youradcloud.com/web/logo-1.png',
-    logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo-.png',    
-    dname: '小胖看房',
+    logo: 'http://mt.youradcloud.com/web/logo-1.png',
+    // logo: 'http://mt.youradcloud.com/web/act/348f7e6ca49f1a18/logo-.png',    
+    dname: '优效',
     icp: '沪ICP备15005792号-2'    
   }
 }
