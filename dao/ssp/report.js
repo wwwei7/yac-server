26dc2c46dba61347e2085d6b21ba6b7427655584
vincent <waydewei@gamil.com>
2017-08-26 11:37:15 +0800

    修复ssp报表sql查询不到adspaceid

--- dao/ssp/report.js
+++ dao/ssp/report.js
@@ -6,7 +6,7 @@ var report = {};
 
 report.findByDay = function(pid, adsid, start, end, next){
 
-    const adsCondition = adsid ? adsid : `SELECT spaceid from adspace WHERE publisherid=${pid}`;
+    const adsCondition = adsid ? adsid : `SELECT id from adspace WHERE publisherid=${pid}`;
 
     var sql = `select sum(shows) as shows , sum(money) as money, sum(click) as click, sum(service_charge) as service, date from log_hour_yax 
     WHERE spaceid in (${adsCondition}) AND date between '${start}' AND '${end}' group by date`;
@@ -24,7 +24,7 @@ report.findByDay = function(pid, adsid, start, end, next){
 
 report.findByHour = function(pid, adsid, day, next){
 
-    const adsCondition = adsid ? adsid : `SELECT spaceid from adspace WHERE publisherid=${pid}`;
+    const adsCondition = adsid ? adsid : `SELECT id from adspace WHERE publisherid=${pid}`;
 
     var sql = `select sum(shows) as shows , sum(money) as money, sum(click) as click, sum(service_charge) as service, date from log_hour_yax 
     WHERE spaceid in (${adsCondition}) AND date = '${day}' group by hour`;
